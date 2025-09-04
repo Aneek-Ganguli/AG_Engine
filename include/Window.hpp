4e2c8aacd333cf3ee85d663e92d673c302378787
Aneek-Ganguli <aneek.ganguli13@gmail.com>
2025-09-04 12:17:31 +0530

    Stuff

--- include/Window.hpp
+++ include/Window.hpp
@@ -59,33 +59,8 @@ private:
     float fov{};
 } Window;
 
-// struct Window createWindow(const char* title,int width,int height);
-// void createGraphicsPipeline(Window* window,float Fov);
-// void newFrame(struct Window *window);
-// void endFrame(struct Window *window);
-// SDL_GPUShader* loadShader(
-// 	Window* window,
-// 	const char* shaderFilename,
-// 	Uint32 samplerCount,
-// 	Uint32 uniformBufferCount,
-// 	Uint32 storageBufferCount,
-// 	Uint32 storageTextureCount
-// );
-// SDL_GPUBuffer *createBuffer(Uint32 size, SDL_GPUBufferUsageFlags usage, struct Window *window);
-// void startCopyPass(struct Window *window);
-// void endCopyPass(struct Window *window);
-// SDL_GPUTransferBuffer *createTransferBuffer(Uint32 size, struct Window *window);
-// void *createTransferMem(SDL_GPUTransferBuffer *transferBuffer, void *data, Uint32 size, struct Window *window);
-// SDL_GPUTransferBufferLocation createTransferBufferLocation(SDL_GPUTransferBuffer *transferBuffer, Uint32 offset);
-// SDL_GPUBufferRegion createBufferRegion(Uint32 size, SDL_GPUBuffer *buffer);
-// void uploadBuffer(SDL_GPUTransferBufferLocation *transferBufferLocation, SDL_GPUBufferRegion *bufferRegion, struct Window *window);
-// SDL_GPUBufferBinding createBufferBinding(SDL_GPUBuffer *buffer);
+
 SDL_Surface* loadImage(const char* imageFilename, int desiredChannels);
-// SDL_GPUTexture* createTexture(SDL_Surface* surface,Window* window);
-// SDL_GPUSampler* createGPUSampler(Window* window);
-// void uploadTexture(SDL_GPUTextureTransferInfo* textureTransferInfo,SDL_GPUTextureRegion* textureRegion,
-// 	Window* window);
-// void cleanUp(Window* window);
-//
+
 
 void print_mat4(mat4 m);
