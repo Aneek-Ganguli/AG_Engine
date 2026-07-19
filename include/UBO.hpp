e48374668b3b26db8bb765bc99834e988be566d4
Aneek-Ganguli <aneek.ganguli13@gmail.com>
2026-07-19 11:45:29 +0530

    Some UBO initialization changes

--- include/UBO.hpp
+++ include/UBO.hpp
@@ -2,6 +2,11 @@
 #include  <glm/glm.hpp>
 
 struct UBO {
+    UBO(){
+        model = glm::mat4(1.0f);
+        view = glm::mat4(1.0f);
+        proj = glm::mat4(1.0f);
+    }
     alignas(16) glm::mat4 model;
     alignas(16) glm::mat4 view;
     alignas(16) glm::mat4 proj;
